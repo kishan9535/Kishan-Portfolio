a1854e6b050e2217b7b9bc97e75c2c1e0eb4cecc
Kishan Kumar N <103923290+kishan9535@users.noreply.github.com>
2024-11-30 11:21:11 +0530

    Update my new Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,6 +7,42 @@ function Projects() {
     <div className="mt-7">
       <Heading>Projects</Heading>
       <Card
+        projectName="News App"
+        techStack="React.js, GNews API, CSS, React Icons"
+        description={
+          <ul className="list-disc list-inside">
+            <li>
+              Developed a responsive and dynamic News Application to provide
+              users with the latest news updates.
+            </li>
+            <li>
+              Integrated the <strong>GNews API</strong> to fetch real-time news
+              categorized into sections such as Technology, Sports, and
+              Business.
+            </li>
+            <li>
+              Built an intuitive and interactive UI with{" "}
+              <strong>React.js</strong>, featuring a dark theme for better user
+              experience.
+            </li>
+            <li>
+              Implemented a <strong>search bar</strong> to find news articles
+              quickly and a category filter for browsing specific topics.
+            </li>
+            <li>
+              Designed the app to be fully responsive and mobile-friendly,
+              ensuring seamless functionality across all devices.
+            </li>
+            <li>
+              Used <strong>React Icons</strong> for visually appealing UI
+              elements and placeholders for missing images.
+            </li>
+          </ul>
+        }
+        codeLink="https://github.com/kishan9535/news-app"
+        liveSiteLink="https://newss24.vercel.app/"
+      />
+      <Card
         projectName="Grocery Store Management"
         techStack="JavaScript, Node.js, Express.js, MongoDB"
         description={
