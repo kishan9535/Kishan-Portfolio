5704026d5f3e59de7cec22cea33b9d4db49e995f
Kishan Kumar N <103923290+kishan9535@users.noreply.github.com>
2025-01-02 19:50:52 +0530

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import kk from "../assets/kk.png";
+import kk from "../assets/kk2.png";
 export default function Logo() {
   return (
-    <div className="w-[100px]">
+   <div className="w-[200px] h-[200px]">
+      {" "}
+      {}
       <img
         src={kk}
         alt="user-photo"
-        className="bg-neutral-800 dark:bg-slate-50 rounded-full border-none"
+        className="bg-neutral-800 dark:bg-slate-50 rounded-full border-none w-full h-full object-cover"
       />
     </div>
   );
